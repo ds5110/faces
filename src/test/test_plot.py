fixed content cropping during cenrot on extreme cases; improved scaling of geometry when not plotting over original image

src/test/test_plot.py
@@ -46,7 +46,8 @@ def annotated_plot(types=None,save_fig=False,n=10):
             )
 
 def plot_stuff(
-        anno,annotate='spline',
+        anno,
+        annotate='spline',
         save_fig=False,
 ):
     plot_image(
@@ -65,25 +66,26 @@ def plot_stuff(
         save_fig=save_fig,
     )
     
-    # # plot geometry without image, to verify coords
-    # plot_image(
-    #     rotated,
-    #     annotate=annotate,
-    #     cross=True,
-    #     grayscale=True,
-    #     skip_img=True,
-    #     save_fig=save_fig,
-    # )
+    # plot geometry without image, to verify coords
+    plot_image(
+        rotated,
+        annotate=annotate,
+        cross=True,
+        grayscale=True,
+        skip_img=True,
+        save_fig=save_fig,
+    )
     
     # # plot coords
+    # row_id = anno.row_id
     # coords = np.stack(
-    #     [tmp[cols].loc[row_id,:].values for cols in cenrot_cols],
+    #     [df[cols].loc[row_id,:].values for cols in cenrot_cols],
     #     1
     # )
     # plot_coords(
     #     coords,
-    #     tmp.loc[row_id,:]['width'],
-    #     tmp.loc[row_id,:]['height'],
+    #     df.loc[row_id,:]['cenrot_width'],
+    #     df.loc[row_id,:]['cenrot_height'],
     #     save_fig=save_fig,
     # )
     
@@ -109,7 +111,28 @@ def plot_stuff(
         save_fig=save_fig,
     )
 
-def test_challenging(code=None,save_fig=False):
+def test_challenging(
+        code=None,
+        all_occurrences=False,
+        save_fig=False,
+):
+    '''
+    loop over images per categorical combinations
+
+    Parameters
+    ----------
+    code : int from [0,16], optional
+        DESCRIPTION. The default is None.
+    all_occurrences : bool, optional
+        whether to loop over all occurrences per combo. The default is False.
+    save_fig : bool, optional
+        whether to save the output. The default is False.
+
+    Returns
+    -------
+    None.
+
+    '''
     codes = range(1,16) if code is None else [code]
     masks = [df[col] == 1 for i, col in enumerate(cat_cols)]
     for combo in [f'{i:b}'.rjust(4,'0') for i in codes]:
@@ -130,8 +153,7 @@ def plot_row_ids(row_ids,desc=None):
     for row_id in row_ids:
         print(f'plotting {row_id}{suff}')
         plot_stuff(
-            cache.get_image(row_id),
-            f'{desc + " " if desc else ""} ({row_id})'
+            cache.get_image(row_id,desc),
         )
 
 def plot_extreme_diffs(save_fig=False):
@@ -170,11 +192,28 @@ def plot_crossing():
     plot_row_ids(sorted(list(row_ids)),'right_cheek')
 
 if __name__ == '__main__':
+    #-- basic usage
     # annotated_plot()
     # annotated_plot(['spline'])
-    test_challenging(11)
-    # test_challenging()
+    # test_challenging(11)
+    # test_challenging(all_occurrences=True)
     # plot_crossing()
+    
+    # #-- spline issues
+    # spline_warn = [
+    #     101,
+    #     159,
+    #     288,
+    #     348,
+    # ]
+    
+    #-- cenrot issue (content out of frame)
+    # crop_issues = [
+    #     118,
+    # ]
+    # plot_row_ids(crop_issues,'crop_issues')
+    
+    #-- junk around testing yaw caclulation
     # rot_fail = [
     #     80,
     #     # 81, # ok
@@ -184,23 +223,23 @@ if __name__ == '__main__':
     #     319,
     #     239,
     # ]
-    
-    # cute = [
-    #     387,
-    # ]
     # rot_fail = [
     #     # 49,
     #     237,
     # ]
     # plot_row_ids(rot_fail,'rot_fail')
-    # plot_row_ids(range(df.shape[0]))
+    plot_row_ids(range(df.shape[0]))
+    # plot_row_ids([6])
     
+    #-- other one-off stuff
+    # cute_crop = [
+    #     387,
+    # ]
     # plot_stuff(
     #     cache.get_image(80),
     #     annotate='scatternum',
     #     save_fig=True
     # )
-    
     # plot_image(
     #     rotate(cache.get_image(80)),
     #     annotate='scatternum',

src/util/plot.py
@@ -16,6 +16,26 @@ from PIL import ImageOps
 
 from util.model import landmark68
 
+def fix_axes(X,Y,ax,flip_y=True):
+    print('setting axes...')
+    data = np.stack([X,Y],axis=1)
+    mins = np.amin(data,axis=0)
+    maxs = np.amax(data,axis=0)
+    extents = maxs - mins
+    max_extent = np.max(extents)
+    for i in range(2):
+        buff = (max_extent - extents[i])/2
+        if i == 0:
+            lim = ax.get_xlim()
+            fun = ax.set_xlim
+        else:
+            lim = ax.get_ylim()
+            fun = ax.set_ylim
+        print(f'setting {"x" if i == 0 else "Y"} lims: {(lim[0]-buff,lim[1]+buff)}')
+        fun((lim[0]-buff,lim[1]+buff))
+    if flip_y:
+        ax.set_ylim(ax.get_ylim()[::-1])
+    
 def plot_image(
         anno,
         points=None,
@@ -141,7 +161,7 @@ def plot_image(
                         fontsize=6,
                     )
     if skip_img:
-        ax.set_ylim(ax.get_ylim()[::-1])
+        fix_axes(X,Y,ax)
     plt.title(title)
     plt.tight_layout()
     if save_fig:
@@ -269,7 +289,8 @@ def plot_coords(
             (X[i], Y[i]),
             fontsize=6,
         )
-    ax.set_ylim(ax.get_ylim()[::-1])
+    
+    fix_axes(X,Y,ax)
     plt.title(title)
     plt.tight_layout()
     if save_fig:

src/util/pre.py
@@ -179,18 +179,20 @@ def get_yaw_data(anno):
 
     Returns
     -------
-    width number
-        image width.
-    height number
-        image height.
+    in_dims 2-item array of number
+        original image dimensions.
+    margin 2-item array of number
+        margin size to fit features such that in_dims + 2*margin = out_dims.
     face : array of number
-        coordinates of the center of the face.
+        original coordinates of the center of the face.
     angle : number
         estimated angle of rotation (in radians).
     coords : number(68,2)
         new landmark coordinates, rotated and centered.
 
     '''
+    # anno = cache.get_image(118)
+    
     # get image and calculate translation
     img = anno.get_image()
     face = anno.get_face_center()
@@ -208,12 +210,29 @@ def get_yaw_data(anno):
     coords = coords@rotx # apply roatation matrix
     coords = coords + center # move coordinates back to the center
     
-    return img.width, img.height, face, angle, coords
+    # calculate buffer
+    mins = np.amin(coords,axis=0)
+    maxs = np.amax(coords,axis=0)
+    in_dims = np.array([img.width, img.height])
+    margin = []
+    for i in range(2):
+        max_buff = 0
+        lo_buff = 0 - mins[i]
+        if lo_buff > max_buff:
+            max_buff = lo_buff
+        hi_buff = maxs[i] - in_dims[i]
+        if hi_buff > max_buff:
+            max_buff = hi_buff
+        margin.append(max_buff + max_buff if max_buff > 0 else 0)
+    
+    margin = np.array(margin)
+    coords += margin
+    return in_dims, margin, face, angle, coords
 
 def rotate(anno):
     def _img():
         img = anno.get_image()
-        _, _, face, angle, coords = get_yaw_data(anno)
+        in_dims, margin, face, angle, coords = get_yaw_data(anno)
         
         # NOTE: We add a buffer around the image
         #       to avoid cropping content during centering
@@ -234,7 +253,14 @@ def rotate(anno):
         )
         
         # crop back to original size
-        crop = rot.crop((img.width,img.height,img.width*2,img.height*2))
+        crop = rot.crop(
+            (
+                img.width - margin[0],
+                img.height - margin[1],
+                img.width*2 + margin[0],
+                img.height*2 + margin[1]
+            )
+        )
         
         return crop
     