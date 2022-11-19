634a0e29ea7c14dbad19a233c1fc549e206a2516
Jesse Hautala <jesse.hautala@gmail.com>
2022-11-18 19:29:02 -0500

    revised README and added 'meta_cache'

---
+++ src/util/meta_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: jhautala
+"""
+
+import pandas as pd
+
+# project
+from .local_cache import LocalCache
+from .alt_cache import AltCache
+
+
+class MetaCache:
+    def __init__(self, baby_cache=AltCache(), adult_cache=LocalCache()):
+        self.baby_cache = baby_cache
+        self.adult_cache = adult_cache
+        df_baby = self.baby_cache.get_meta('decorated')
+        df_adult = self.adult_cache.get_meta('decorated')
+        df_baby['baby'] = 1
+        df_adult['baby'] = 0
+        self.meta = pd.concat([df_baby, df_adult], ignore_index=True)
+
+    def get_image(self, row_id=0, baby=None):
+        if baby is None:
+            baby = self.meta.loc[row_id]['baby']
+        if baby:
+            return self.baby_cache.get_image(row_id)
+        else:
+            return self.adult_cache.get_image(row_id)
