b332c3f43abd214e3a889aa57c42dd9b7f40f02d
sophiacofone <sophiacofone@gmail.com>
2022-11-19 23:50:15 -0500

    started adding ways to pull columns

--- soph/read_data.py
+++ soph/read_data.py
@@ -14,8 +14,20 @@ def get_data():
 
     return df
 
+def get_categories(df):
+    main_predictors = ['boxratio', 'interoc','interoc_norm','boxsize','boxsize/interoc']
+    df1 = df.loc[:, df.columns.str.startswith('norm_cenrot_sym_diff')]
+    norm_cenrot_sym_diff = list(df1.columns)
+    # dim = 'x'
+    # norm_cenrot_sym_diff=['norm_cenrot_sym_diff-{}{}'.format(dim,i) for i in range(67)]
+    # dim = 'y'
+    # norm_cenrot_sym_diff = norm_cenrot_sym_diff+['norm_cenrot_sym_diff-{}{}'.format(dim,i) for i in range(68)]
+    
+    return main_predictors, norm_cenrot_sym_diff
+
 def main():
     df = get_data()
+    main_predictors, norm_cenrot_sym_diff = get_categories(df)
 
 if __name__ == "__main__":
     main()
