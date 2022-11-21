88478a4773a8d7b5f6bddb62f0f954522866b8bf
sophiacofone <sophiacofone@gmail.com>
2022-11-21 14:35:31 -0500

    cleanup and renaming

--- soph/dist_metadata.py
+++ soph/dist_metadata.py
@@ -2,32 +2,32 @@
 '''
 This file is intended to calculate additional meta-data for computing the euclidean distances between the coordinate points.
 We use the normalized to the bounding box data as the coord input, then compute the distance between each point and every other point.
-The output is an updated CSV file.
+The output is an updated df, but the  "main" function below puts it into a CSV>.
 '''
 
-import numpy as np
+#project
 from read_data import get_data
-
+#basics
 import pandas as pd
 import numpy as np
+#sklearn
 from sklearn.metrics.pairwise import euclidean_distances as edist
 
 def get_dist_df(df_cr,f_num_list = range(68)):
     """ 
-    df_cr = df.loc[:,df.columns.str.startswith('norm_cenrot-')]
+    df input should be a subet of the main one. Below I used the "norm-" derived columns but I've also done it with "norm_cenrot-".
     f_num_list = the list of feature (coordinates) numbers to compute the distances betweeen, 
     ex. range(68) for all of them, or [1,5,6,20,30,55]
     """
-    #f_num_list = range(68)
 
-    # create the headers for the distances. need to make sure not to include distance between f1-f2 and f2-f1
+    #Create the headers for the distances. Need to make sure not to include distance between f1-f2 and f2-f1 (duplicates).
     dist_labels = []
     for i in f_num_list:
         for j in f_num_list:
             if ('dist_{}_{}'.format(i,j) not in dist_labels) and (('dist_{}_{}'.format(j,i) not in dist_labels)) and i!=j:
                 dist_labels.append('dist_{}_{}'.format(i,j))
 
-    # create matrix with row=images,cols=distances
+    #Create matrix with row=images,cols=distances
     num_feat = len(f_num_list)
     im_dist_mat = np.zeros((df_cr.shape[0],int(num_feat*(num_feat-1)/2)))
 
@@ -39,21 +39,24 @@ def get_dist_df(df_cr,f_num_list = range(68)):
             y = anot_data['norm-y{}'.format(coord)]
             anot_coords.append(np.array([x,y]))
 
-        # get the pairwise euclidian distance between all points
+        #Get the pairwise euclidian distance between all points
         dist_mat = edist(anot_coords)
-        # get a flattened array of the pairwise distances, remove the diogonal since self distances are 0.
+        #Get a flattened array of the pairwise distances, remove the diagonal since self distances are 0
         dist_list = dist_mat[np.triu_indices(dist_mat.shape[0],k = 1)]
-        # insert the distances for this image into the master matrix
+        #Insert the distances for this image into the master matrix
         im_dist_mat[image_index,:] = dist_list
         
     # create and return the data_frame of the distances
-    #df_dist = pd.DataFrame(im_dist_mat,columns = dist_labels)
     return pd.DataFrame(im_dist_mat,columns = dist_labels)
 
 def main():
+    #grabbing main DF
     df = get_data()
+    #getting subset of DF
     df_cr = df.loc[:,df.columns.str.startswith('norm-')]
+    #calculating new df
     df_dist = get_dist_df(df_cr,f_num_list = range(68))
+    #adding it to the main df and outputting to csv
     df = pd.concat([df.copy(),df_dist],axis=1)
     df.to_csv('merged_landmarks_dist.csv')
 
