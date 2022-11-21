5194e245fede684e9991f9cc73c89df0f11e120f
sophiacofone <sophiacofone@gmail.com>
2022-11-20 20:14:40 -0500

    cleanup logreg vs logreg testing

--- soph/logreg.py
+++ soph/logreg.py
@@ -1,12 +1,8 @@
 #Sophia Cofone 11/19 
-#File is intended for trying out logistic regression
+#File is intended for implementing logistic regression
 
-from read_data import get_data, get_categories
-from helpers import get_Xy, tt_split, plot_cm, class_report
-from imbalanced_resampling import upsample, downsample
+from helpers import tt_split
 
-import pandas as pd
-import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -15,12 +11,10 @@ from sklearn.feature_selection import RFECV
 from sklearn.model_selection import StratifiedKFold
 
 
-
-
 def logreg(X,y):
     y_flat = np.ravel(y)
     Xtrain, Xtest, ytrain, ytest = tt_split(X,y_flat)
-    logreg = LogisticRegression()
+    logreg = LogisticRegression(max_iter=1000)
     fitted = logreg.fit(Xtrain, ytrain)
     y_pred = fitted.predict(Xtest)
     print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logreg.score(Xtest, ytest)))
@@ -31,7 +25,7 @@ def feature_selection(X,y):
     y_flat = np.ravel(y)
     min_features_to_select = 1  # Minimum number of features to consider
     rfecv = RFECV(
-        estimator=LogisticRegression(),
+        estimator=LogisticRegression(max_iter=1000),
         step=1,
         cv=StratifiedKFold(2),
         scoring="accuracy",
@@ -54,54 +48,3 @@ def feature_selection(X,y):
     plt.show()
 
     return rfecv.support_
-
-def main():
-    df = get_data()
-    main_predictors, norm_cenrot_sym_diff = get_categories(df)
-    print(norm_cenrot_sym_diff)
-    #selecting certain columns as predictors:
-    predictors_list = norm_cenrot_sym_diff
-    # predictors_list = ['norm_cenrot_sym_diff-x36', 'norm_cenrot_sym_diff-y36', 'norm_cenrot_sym_diff-x39', 'norm_cenrot_sym_diff-y39', 'norm_cenrot_sym_diff-x37', 'norm_cenrot_sym_diff-y37', 'norm_cenrot_sym_diff-x38', 'norm_cenrot_sym_diff-y38', 'norm_cenrot_sym_diff-x40', 'norm_cenrot_sym_diff-y40', 'norm_cenrot_sym_diff-x41', 'norm_cenrot_sym_diff-y41', 'norm_cenrot_sym_diff-x31', 'norm_cenrot_sym_diff-y31', 'norm_cenrot_sym_diff-x32', 'norm_cenrot_sym_diff-y32', 'norm_cenrot_sym_diff-x0', 'norm_cenrot_sym_diff-y0', 'norm_cenrot_sym_diff-x1', 'norm_cenrot_sym_diff-y1', 'norm_cenrot_sym_diff-x2', 'norm_cenrot_sym_diff-y2', 'norm_cenrot_sym_diff-x3', 'norm_cenrot_sym_diff-y3', 'norm_cenrot_sym_diff-x4', 'norm_cenrot_sym_diff-y4', 'norm_cenrot_sym_diff-x5', 'norm_cenrot_sym_diff-y5', 'norm_cenrot_sym_diff-x6', 'norm_cenrot_sym_diff-y6', 'norm_cenrot_sym_diff-x7', 'norm_cenrot_sym_diff-y7', 'norm_cenrot_sym_diff-x17', 'norm_cenrot_sym_diff-y17', 'norm_cenrot_sym_diff-x18', 'norm_cenrot_sym_diff-y18', 'norm_cenrot_sym_diff-x19', 'norm_cenrot_sym_diff-y19', 'norm_cenrot_sym_diff-x20', 'norm_cenrot_sym_diff-y20', 'norm_cenrot_sym_diff-x21', 'norm_cenrot_sym_diff-y21', 'norm_cenrot_sym_diff-x48', 'norm_cenrot_sym_diff-y48', 'norm_cenrot_sym_diff-x49', 'norm_cenrot_sym_diff-y49', 'norm_cenrot_sym_diff-x50', 'norm_cenrot_sym_diff-y50', 'norm_cenrot_sym_diff-x60', 'norm_cenrot_sym_diff-y60', 'norm_cenrot_sym_diff-x61', 'norm_cenrot_sym_diff-y61', 'norm_cenrot_sym_diff-x67', 'norm_cenrot_sym_diff-y67', 'norm_cenrot_sym_diff-x59', 'norm_cenrot_sym_diff-y59', 'norm_cenrot_sym_diff-x58', 'norm_cenrot_sym_diff-y58']
-    
-    # print('with without feature selection')
-    # X,y = get_Xy(df,predictors_list)
-    # y_pred, ytest, fitted = logreg(X,y)
-    # plot_cm(ytest,y_pred,'logreg')
-    # class_report(ytest,y_pred,'logreg')
-
-    #trying logreg with feature selection
-    #feature selection
-    X,y = get_Xy(df,predictors_list)
-    bool_features = feature_selection(X,y)
-    selected_features = [i for (i, v) in zip(predictors_list,list(bool_features)) if v]
-
-    # print('with feature selection (2)')
-    # #logreg
-    # predictors_list = selected_features
-    # X,y = get_Xy(df,predictors_list)
-    # y_pred, ytest, fitted = logreg(X,y)
-    # plot_cm(ytest,y_pred,'logreg')
-    # class_report(ytest,y_pred,'logreg')
-
-    # #logreg
-    # print('with feature selection (1)')
-    # predictors_list = [selected_features[0]]
-    # X,y = get_Xy(df,predictors_list)
-    # y_pred, ytest, fitted = logreg(X,y)
-    # plot_cm(ytest,y_pred,'logreg')
-    # class_report(ytest,y_pred,'logreg')
-
-
-
-    #logreg with feature selection and downsampled
-    predictors_list = selected_features
-    upsample_df = downsample(df)
-    X,y = get_Xy(upsample_df,predictors_list)
-    y_pred, ytest, fitted = logreg(X,y)
-    plot_cm(ytest,y_pred,'logreg')
-    class_report(ytest,y_pred,'logreg')
-
-
-
-if __name__ == "__main__":
-    main()
---
+++ soph/logreg_ex.py
@@ -0,0 +1,70 @@
+#Sophia Cofone 11/20 
+#File is intended for trying out logistic regression
+
+from read_data import get_data, get_categories
+from helpers import get_Xy, plot_cm, class_report
+from imbalanced_resampling import upsample, downsample
+from logreg import logreg, feature_selection
+
+def test_partition_a(df,predictors_list):
+    print('Without feature selection')
+    X,y = get_Xy(df,predictors_list)
+    y_pred, ytest, fitted = logreg(X,y)
+    plot_cm(ytest,y_pred,'logreg')
+    class_report(ytest,y_pred,'logreg')
+    
+    print('Feature selection')
+    X,y = get_Xy(df,predictors_list)
+    bool_features = feature_selection(X,y)
+    selected_features = [i for (i, v) in zip(predictors_list,list(bool_features)) if v]
+
+    return selected_features
+    
+def test_partition_b(df,selected_features):
+    print('With feature selection')
+    predictors_list = selected_features
+    X,y = get_Xy(df,predictors_list)
+    y_pred, ytest, fitted = logreg(X,y)
+    plot_cm(ytest,y_pred,'logreg')
+    class_report(ytest,y_pred,'logreg')
+
+    print('With feature selection and downsampled')
+    upsample_df = downsample(df)
+    X,y = get_Xy(upsample_df,predictors_list)
+    y_pred, ytest, fitted = logreg(X,y)
+    plot_cm(ytest,y_pred,'logreg')
+    class_report(ytest,y_pred,'logreg')
+
+def main():
+    df = get_data()
+    _, angle_off, main_predictors,norm_cenrot_sym_diff, norm_cenrot,_,_ = get_categories(df)
+
+    print('partition 1')
+    selected_features = test_partition_a(df,main_predictors) 
+    test_partition_b(df,selected_features)
+
+    print('partition 2')
+    selected_features = test_partition_a(df,main_predictors) 
+    test_partition_b(df,selected_features)
+    #choosing 1
+    selected_features = selected_features[0:1]
+    test_partition_b(df,selected_features)
+
+    print('partition 3')
+    selected_features = test_partition_a(df,main_predictors+angle_off) 
+    #choosing 2
+    selected_features = selected_features[0:2]
+    test_partition_b(df,selected_features)
+
+    print('partition 4')
+    selected_features = test_partition_a(df,norm_cenrot) 
+    #choosing 66
+    selected_features = selected_features[0:66]
+    test_partition_b(df,selected_features)
+
+    print('partition 5')
+    selected_features = test_partition_a(df,norm_cenrot_sym_diff)
+    test_partition_b(df,selected_features)
+
+if __name__ == "__main__":
+    main()
