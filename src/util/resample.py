9331549e0040c7e8107f8bcbb01b2b89a56f7ed0
sophiacofone <sophiacofone@gmail.com>
2022-11-21 23:11:05 -0500

    rename resampling

--- src/util/resample.py
+++ src/util/resample.py
@@ -9,9 +9,7 @@ We will use logistic regression to see the differences in the sampling methods (
 
 reference https://elitedatascience.com/imbalanced-classes
 '''
-#project
-from helpers import get_data
-from helpers import get_Xy, class_report
+
 #basic
 import pandas as pd
 #sklearn
@@ -64,31 +62,3 @@ def downsample(df):
     print(f'Resulting instances of class 0 and 1 after upsampling: {counts}')
 
     return df_downsampled
-    
-def main():
-    from logreg_ex import logreg
-    #testing out the different sampling options with logreg
-    #get data
-    df = get_data()
-    predictors_list = ['boxratio']
-
-    #testing
-    print('Trying with no sampling changes (unbalanced)')
-    X,y = get_Xy(df,predictors_list)
-    y_pred, ytest, fitted = logreg(X,y)
-    class_report(ytest,y_pred,'logreg')
-
-    print('Trying with upsampling')
-    upsample_df = upsample(df)
-    X,y = get_Xy(upsample_df,predictors_list)
-    y_pred, ytest, fitted = logreg(X,y)
-    class_report(ytest,y_pred,'logreg')
-
-    print('Trying with downsampling')
-    downsample_df = downsample(df)
-    X,y = get_Xy(downsample_df,predictors_list)
-    y_pred, ytest, fitted = logreg(X,y)
-    class_report(ytest,y_pred,'logreg')
-
-if __name__ == "__main__":
-    main()
