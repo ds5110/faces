99c2d55fbf15d585991307cfc6cf4ab8216b6ea1
ZongyuWu97 <zongyuwu97@gmail.com>
2022-12-02 16:33:14 -0500

    add plots and classification report

--- src/util/zw_svc_geometric_value.py
+++ src/util/zw_svc_geometric_value.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.pipeline import make_pipeline
@@ -17,24 +18,25 @@ X_train, X_test, y_train, y_test = train_test_split(X_geometric, y,
 # Train on geometric values
 svc_geometric = SVC(class_weight='balanced')
 
-model_original = make_pipeline(svc_geometric)
+model_geometric = make_pipeline(svc_geometric)
 
-param_grid_original = {'svc__C': [50, 100, 500, 1000],
+param_grid_geometric = {'svc__C': [50, 100, 500, 1000],
 					   'svc__gamma': [0.05, 0.1, 0.5, 1, 5],
 					   'svc__kernel': ['rbf','linear', 'poly']}
 
-grid_original = GridSearchCV(model_original, param_grid_original, return_train_score=True, n_jobs=5, refit=True)
-grid_original.fit(X_train, y_train)
+grid_geometric = GridSearchCV(model_geometric, param_grid_geometric, return_train_score=True, n_jobs=5, refit=True)
+grid_geometric.fit(X_train, y_train)
 
-best_params_ = grid_original.best_params_
+best_params_ = grid_geometric.best_params_
 print(best_params_)
-print(f"best train score: {grid_original.best_score_:.3f}")
+print(f"best train score: {grid_geometric.best_score_:.3f}\n")
 
 # Predict
-model = grid_original.best_estimator_
-y_predict = model.predict(X_test)
+model_best = grid_geometric.best_estimator_
+y_predict = model_best.predict(X_test)
 
 # Get classification report
+print('Classification report for model using geometric values:\n')
 print(classification_report(y_test, y_predict, target_names=['adult', 'infant']))
 
 # Plot confusion matrix
@@ -60,18 +62,31 @@ Support_vector_index = svc_trained.support_
 
 # Plot geometric values and support vectors
 X_geometric['baby'] = y
-sns.lmplot(data=X_geometric, x='boxratio', y='interoc_norm',
-		   hue='baby',
-		   legend=True,
-		   fit_reg=False)
+sns.scatterplot(data=X_geometric, x='boxratio', y='interoc_norm',
+				hue='baby',
+				legend=True)
 plt.savefig('figs/Geometric_values')
 plt.show()
 
-plt.scatter(X_train['boxratio'], X_train['interoc_norm'],
-			s=100,
-			c='k',
-			alpha=0.1,
-			linewidth=1.5,
-			edgecolor='red')
+support_vector = X_train.iloc[Support_vector_index][:]
+support_vector['baby'] = y_train.iloc[Support_vector_index][:]
+sns.scatterplot(data=support_vector, x='boxratio', y='interoc_norm',
+				hue='baby',
+				legend=True)
 plt.savefig('figs/Support_Vectors')
+plt.show()
+
+# Plot heatmap for geometric values
+interoc_norm = np.linspace(0.1, 0.9, 100)
+boxratio = np.linspace(0.7, 1.7, 100)
+heatmap = [['.'] * len(boxratio) for _ in range(len(interoc_norm))]
+
+for i in range(len(interoc_norm)):
+	for j in range(len(boxratio)):
+		heatmap[i][j] = model_best.predict([[boxratio[j], interoc_norm[i]]])[0]
+heatmap = pd.DataFrame(heatmap)
+
+sns.heatmap(data=heatmap,
+			cbar-False)
+plt.savefig('figs/SVC_meshgrid_of_geometric')
 plt.show()
