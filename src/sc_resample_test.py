aa88f02275ac4e45d5453ac31e4bcfd8470568a1
sophiacofone <sophiacofone@gmail.com>
2022-12-06 11:57:23 -0500

    fix to resample code

--- src/sc_resample_test.py
+++ src/sc_resample_test.py
@@ -17,19 +17,19 @@ def main():
     #testing
     print('Trying with no sampling changes (unbalanced)')
     X,y = get_Xy(df,predictors_list)
-    Xtest, ytest, fitted, y_pred = logreg(X,y)
+    Xtrain, Xtest, ytrain, ytest, fitted, y_pred = logreg(X,y)
     class_report(ytest,y_pred,'logreg')
 
     print('Trying with upsampling')
     upsample_df = upsample(df)
     X,y = get_Xy(upsample_df,predictors_list)
-    Xtest, ytest, fitted, y_pred = logreg(X,y)
+    Xtrain, Xtest, ytrain, ytest, fitted, y_pred = logreg(X,y)
     class_report(ytest,y_pred,'logreg')
 
     print('Trying with downsampling')
     downsample_df = downsample(df)
     X,y = get_Xy(downsample_df,predictors_list)
-    Xtest, ytest, fitted, y_pred = logreg(X,y)
+    Xtrain, Xtest, ytrain, ytest, fitted, y_pred = logreg(X,y)
     class_report(ytest,y_pred,'logreg')
 
 if __name__ == "__main__":
